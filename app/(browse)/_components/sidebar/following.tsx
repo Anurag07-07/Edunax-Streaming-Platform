0d4f8dd6f6220416da83518a43f56c3e5e6d119a
Anurag Raj <adityanv8@gmail.com>
2025-10-29 03:26:53 +0530

    completed verify Token

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { useSidebar } from "@/store/use-sidebar"
-import { Follow, Stream, User } from "@prisma/client"
+import { Follow,  User } from "@prisma/client"
 import { UserItem, UserItemSkeleton } from "./UserItem"
 
-interface FollowingProps{
-  data:(Follow & {following:User & {
-    stream:Stream
-  }})[]
+interface FollowingProps {
+  data: (Follow & {
+    following: User & {
+      stream: {isLive:boolean} | null
+    }
+  })[]
 }
 
 export const Following = ({data}:FollowingProps)=>{
@@ -35,7 +37,7 @@ export const Following = ({data}:FollowingProps)=>{
           key={follow.following.id}
           username={follow.following.username}
           imageUrl={follow.following.imageUrl}
-          isLive={follow.following.stream.isLive}
+          isLive={follow.following.stream!.isLive}
           >
           </UserItem>
         ))
--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,8 +1,32 @@
+import { StreamPlayer } from '@/components/stream-player';
+import { getUserByUsername } from '@/lib/user-service';
+import { currentUser } from '@clerk/nextjs/server';
 import React from 'react'
 
-const CreatorPage = () => {
+interface CreatorPageProps{
+  params:Promise<{username:string}>
+}
+
+const CreatorPage = async({
+  params
+}:CreatorPageProps) => {
+
+  const {username} = (await params);
+  const externalUser = await currentUser()
+  const user = await getUserByUsername(username) 
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized")
+  }
+
   return (
-    <div>CreatorPage</div>
+    <div className=' h-full'>
+      <StreamPlayer
+      user={user}
+      stream={user.stream}
+      isFollowing
+      ></StreamPlayer>
+    </div>
   )
 }
 
