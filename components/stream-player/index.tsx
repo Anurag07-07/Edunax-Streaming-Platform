08e76a0b8028ec07dcc3c41bdf1c3b629ab961bf
Anurag Raj <adityanv8@gmail.com>
2025-11-01 23:46:54 +0530

    completed chat section

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -1,56 +1,80 @@
-'use client'
-import { useViewerToken } from '@/hooks/use_viewer_token';
-import { Stream, User } from '@prisma/client'
-import React from 'react'
-import style from './stream.module.css'
+"use client"
 
-import {LiveKitRoom} from '@livekit/components-react' 
-import { cn } from '@/lib/utils';
-import { Video } from './video';
-
-const NEXT_PUBLIC_LIVEKIT_WS_URL  = process.env.NEXT_PUBLIC_LIVEKIT_WS_URL!
-
-console.log(NEXT_PUBLIC_LIVEKIT_WS_URL);
-
+import { useViewerToken } from "@/hooks/use_viewer_token";
+import { Stream, User } from "@prisma/client"
+import {LiveKitRoom} from "@livekit/components-react";
+import { Video } from "./video";
+import { cn } from "@/lib/utils"
+import { useChatSidebar } from "@/store/use-chat-sidebar"
+import { Chat } from "./chat";
+import { ChatToggle } from "./chat-toggle";
 
 interface StreamPlayerProps{
-  user:User & {stream:Stream |null };
-  stream:Stream,
-  isFollowing:boolean
+    user:User & {stream:Stream | null};
+    stream:Stream;
+    isFollowing:boolean;
 }
 
 export const StreamPlayer = ({
-  user,
-  stream,
-  isFollowing
+    user,
+    stream,
+    isFollowing
 }:StreamPlayerProps) => {
+    const {
+        token,
+        name,
+        identity
+    } = useViewerToken(user.id);
 
-  const {token,name,identity} = useViewerToken(user.id)
+    const {collapsed}=useChatSidebar();
 
-  if (!token || !name || !identity) {
+    // console.log(identity)
+    
+    if(!token || !name || !identity){
+        return(
+            <div>
+                Cannot watch the stram
+            </div>
+        )
+    }
+    
     return (
-      <div>
-        Cannot watch the stream
-      </div>
-    )
-  }
-
-  return (
-    <>
-      <LiveKitRoom
-       token={token}
-       serverUrl={NEXT_PUBLIC_LIVEKIT_WS_URL}
-       className=' grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full'
-       audio
-       video
-      >
-        <div className={cn(`space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10`,style)}>
-          <Video
-           hostname={user.username}
-           hostIdentity={user.id}
-          ></Video>
+        <div>
+          {
+            collapsed && (
+              <div className=" hidden lg:block fixed top-[100px] right-2 z-50">
+                <ChatToggle></ChatToggle>
+              </div>
+            )
+          }
+            <LiveKitRoom
+                token={token}
+                serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+                className={cn("grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+                    collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+                )}
+            >
+                <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+                    <Video 
+                        hostname={user.username}
+                        hostIdentity={user.id}
+                    />
+                </div>
+                <div className={cn(
+                    "col-span-1",
+                    collapsed && "hidden"
+                )}>
+                    <Chat 
+                        viewerName={name}
+                        hostName={user.username}
+                        hostIdentity={user.id}
+                        isFollowing={isFollowing}
+                        isChatEnabled={stream.isChatEnabled}
+                        isChatDelayed={stream.isChatDelayed}
+                        isChatFollowersOnly={stream.isChatFollowersOnly}
+                    />
+                </div>
+            </LiveKitRoom>
         </div>
-      </LiveKitRoom>
-    </>
-  )
+    )
 }
