853094875a3445dec6b7f73b571c796078f289dd
Anurag Raj <adityanv8@gmail.com>
2025-11-09 12:56:27 +0530

    SearchPage  Completed

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from "react";
-import {Results, ResultSkeleton} from "./_components/Results";
+import {Results, ResultsSkeleton} from "./_components/Results";
 
 export default function Page() {
   return <div className=" h-full max-w-screen-2xl mx-auto">
-    <Suspense fallback={<ResultSkeleton></ResultSkeleton>}>
+    <Suspense fallback={<ResultsSkeleton></ResultsSkeleton>}>
       <Results></Results>
     </Suspense>
   </div>
