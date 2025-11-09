208a4233b89ae848448a54234b2b5844c4b8c685
Anurag Raj <adityanv8@gmail.com>
2025-11-10 00:49:34 +0530

    completed

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,15 +1,15 @@
+import { StreamPlayer } from "@/components/stream-player"
+import { isBlockedByUser, isBlockingUser } from "@/lib/block-service"
 import { isFollowing } from "@/lib/follow-service"
 import { getUserByUsername } from "@/lib/user-service"
 import { notFound } from "next/navigation"
-import { isBlockedByUser, isBlockingUser } from "@/lib/block-service"
-import { StreamPlayer } from "@/components/stream-player"
 
 interface UserPageProps{
   params:{username:string}
 }
 
 const UserPage = async({params}:UserPageProps) => {
-  const {username} = (await params)
+  const {username} = params
   const user = await getUserByUsername(username)
 
   if (!user || !user.stream) {
