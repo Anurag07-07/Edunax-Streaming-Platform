4a0168728af78ef084045f7a1604fe2e6b65ce18
Anurag Raj <adityanv8@gmail.com>
2025-11-10 00:58:33 +0530

    changes

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,35 +1,35 @@
-import { StreamPlayer } from "@/components/stream-player"
-import { isBlockedByUser, isBlockingUser } from "@/lib/block-service"
-import { isFollowing } from "@/lib/follow-service"
-import { getUserByUsername } from "@/lib/user-service"
-import { notFound } from "next/navigation"
+import { getUserByUsername } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+import { StreamPlayer } from "@/components/stream-player";
+import { isFollowing } from "@/lib/follow-service";
+import { isBlockedByUser } from "@/lib/block-service";
 
-interface UserPageProps{
-  params:{username:string}
-}
+export default async function UserPage({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const { username } = params;
 
-const UserPage = async({params}:UserPageProps) => {
-  const {username} = params
-  const user = await getUserByUsername(username)
+  if (!username) notFound();
 
-  if (!user || !user.stream) {
-    notFound()
-  }
+  const user = await getUserByUsername(username);
+  if (!user || !user.stream) notFound();
 
-  
-  const isFollow = await isFollowing(user.id)
-  const isBlocked = await isBlockedByUser(user.id)
+  const [isFollow, isBlocked] = await Promise.all([
+    isFollowing(user.id!),
+    isBlockedByUser(user.id),
+  ]);
 
-  // ➡️ Check if the VIEWER has blocked the profile owner (UI State)
-    const isBlocking = await isBlockingUser(user.id)
-
-  if (isBlocked) {
-    notFound()
-  }
+  if (isBlocked) notFound();
 
   return (
-    <StreamPlayer user={user} stream={user.stream} isFollowing={isFollow}></StreamPlayer>
-  )
+    <div>
+      <StreamPlayer
+        user={user}
+        stream={user.stream}
+        isFollowing={isFollow}
+      />
+    </div>
+  );
 }
-
-export default UserPage
